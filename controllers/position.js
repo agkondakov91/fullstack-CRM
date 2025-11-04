68e28b5a7cac1d3f332d18a3bad31f84bd6e89e9
Alexander Kondakov <agkondakov91@gmail.com>
2025-11-04 18:44:31 +0300

    feat05-2: create all fn in position controller)

--- controllers/position.js
+++ controllers/position.js
@@ -16,6 +16,13 @@ class ControllerPosition {
 
   async create(req, res) {
     try {
+      const position = new Position({
+        name: req.body.name,
+        cost: req.body.cost,
+        category: req.body.category,
+        user: req.user.id,
+      }).save();
+      res.status(201).json(position);
     } catch (error) {
       errorHandler(res, error);
     }
@@ -23,6 +30,10 @@ class ControllerPosition {
 
   async remove(req, res) {
     try {
+      await Position.remove({ _id: req.params.id });
+      res.status(200).json({
+        message: "Position deleted",
+      });
     } catch (error) {
       errorHandler(res, error);
     }
@@ -30,6 +41,12 @@ class ControllerPosition {
 
   async update(req, res) {
     try {
+      const position = await Position.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(position);
     } catch (error) {
       errorHandler(res, error);
     }
