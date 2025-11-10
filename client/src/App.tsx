3de82202310b96a155f3c12649d8945e7935b26a
Alexander Kondakov <agkondakov91@gmail.com>
2025-11-10 22:37:18 +0300

    feat_client-01: add materialeze

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 const App = () => {
   return (
     <>
-      <h1>FrontEnd Client</h1>
+      <h1>Working</h1>
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Card Title</span>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles.css'
 import App from './App.tsx'
+import "materialize-css/dist/js/materialize.js"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
